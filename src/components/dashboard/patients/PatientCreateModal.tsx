c3c85c9b144c74dac2984937b5f74075afffd067
Angel Romero <144820426+aeRomeroz@users.noreply.github.com>
2026-02-26 15:39:03 +0000

    feat(patient): create modal template for patient creation

---
+++ src/components/dashboard/patients/PatientCreateModal.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import Modal from "../../ui/Modal"
+import Button from "../../ui/Button"
+import { PATIENT_STATUS, type Status } from "../../../types/patients"
+
+interface PatientCreateModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+export default function PatientCreateModal() {
+    return (
+        <div></div>
+    )
+}
